b7e9e85f8ea6a3145a1193ff8b4413c9902727b4
Dhananjay-JSR <ajaysenday@gmail.com>
2023-12-02 14:47:47 +0530

    Added All Sections

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { Outfit, Inter } from "next/font/google";
 import ClientComponnet from "./components/Page/ClientComponents/ClientHolder";
 import ROver from "./components/Page/ClientComponents/Modules/WOrkSample";
+import Link from "next/link";
 const outfir = Outfit({
   subsets: ["latin"],
 });
@@ -636,60 +637,232 @@ function Testimony() {
   );
 }
 
-function Closing(){
-  return <div className=" flex py-[100px] px-[30px] max-w-[1000px] 2xl:max-w-[1200px] mx-10 rounded-2xl border lg:mx-auto bg-white">
-      <div className="w-full">
+function HappyCustomerContainer() {
+  return (
+    <div className="mx-auto relative w-fit">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="absolute w-96 -translate-x-1/2 left-1/2 -translate-y-1/2 top-1/2"
+        viewBox="0 0 364 108"
+        width={200}
+        height={100}
+      >
+        <defs>
+          <filter
+            id="a"
+            width="159.2%"
+            height="346.7%"
+            x="-29.8%"
+            y="-124%"
+            filterUnits="objectBoundingBox"
+          >
+            <feOffset in="SourceAlpha" result="a1016z" />
+            <feGaussianBlur in="a1016z" result="a1017z" stdDeviation="14.825" />
+            <feColorMatrix
+              color-interpolation-filters="sRGB"
+              in="a1017z"
+              result="a1018z"
+              values="0 0 0 0 0.929 0 0 0 0 0.929 0 0 0 0 0.98 0 0 0 1 0"
+            />
+          </filter>
+          <path
+            id="b"
+            d="M64.056 41.019c0-6.295 5.152-11.398 11.507-11.398h212.874c6.355 0 11.507 5.103 11.507 11.398v27.117c0 6.295-5.152 11.398-11.507 11.398h-100.08L182 85.659l-6.447-6.125h-99.99c-6.355 0-11.507-5.103-11.507-11.398Z"
+          />
+        </defs>
+        <g filter="url(#a)">
+          <use
+            stroke="#000"
+            stroke-miterlimit="10"
+            stroke-width="1.48"
+            clipPath="url(#a1013z)"
+            href="#b"
+          />
+        </g>
+        <use
+          fill=" rgb(255, 255, 255)"
+          stroke=" rgb(237, 237, 250)"
+          stroke-miterlimit="10"
+          stroke-width="1.48"
+          clipPath="url(#a1013z)"
+          href="#b"
+        />
+      </svg>
       <div
         className={
-          "w-fit   text-[#8247ff] font-medium text-sm whitespace-pre bg-white px-2 py-1 rounded-md border-[#8247ff33] border " +
+          "text-[25px] relative z-20 font-semibold  left-0 -top-1/2     " +
           outfir.className
         }
       >
-        <span className="text-base mr-2">👋</span>
-        {/* eslint-disable-next-line react/no-unescaped-entities */}
-        DON'T MISS OUT
+        <span className="text-[#8247ff]">200+</span> Happy users
       </div>
-      <h5 className={"text-[45px] 2xl:text-[56px] font-medium w-[500px] leading-none mt-6 "+outfir.className}>
-      Unleash your <span className="text-[#fe8162]">Potential</span> with us
-      </h5>
-      <div className={"text-[#767575] text-[20px] font-medium w-[350px] mt-6 "+inter.className}>
-      Join our community of ambitious individuals and organizations eager to make a difference.
-      </div>
-      <div className="mt-6">
-      <button
-        className={
-          outfir.className +
-          " bg-[#8247ff] relative w-[220px] group justify-center flex rounded-xl text-white font-semibold text-xl px-7 py-3 transition-all  hover:shadow-[0px_0px_0px_10px_#d7c5fc]  "
-        }
-      >
-        <div className="text-center group-hover:-translate-x-4 transition-all ease-in-out duration-300 ">
-          Try Out Now
-        </div>
-        <div className="hidden group-hover:block absolute right-9">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="25"
-            height="25"
-            fill="currentColor"
-            className="bi bi-arrow-right-circle-fill"
-            viewBox="0 0 16 16"
-          >
-            <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z" />
-          </svg>
-        </div>
-      </button>
-      </div>
-      </div>
-      <div className="w-full">
-
-      </div>
-  </div>
+    </div>
+  );
 }
 
-function Footer(){
-  return <div className="bg-gradient-to-b from-white to-[#fdf2ed]">
-    <Closing/>
-  </div>
+function Closing() {
+  return (
+    <div className=" flex justify-center flex-col 2xl:flex-row py-[100px] px-[30px] max-w-[1000px] 2xl:max-w-[1200px] mx-10 rounded-2xl border lg:mx-auto bg-white">
+      <div className="w-fit mx-auto">
+        <div
+          className={
+            "w-fit 2xl:mx-0 mx-auto  text-[#8247ff] font-medium text-sm whitespace-pre bg-white px-2 py-1 rounded-md border-[#8247ff33] border " +
+            outfir.className
+          }
+        >
+          <span className="text-base mr-2">👋</span>
+          {/* eslint-disable-next-line react/no-unescaped-entities */}
+          DON'T MISS OUT
+        </div>
+        <h5
+          className={
+            "text-[45px] 2xl:text-[56px] font-medium 2xl:w-[500px] leading-none mt-6 " +
+            outfir.className
+          }
+        >
+          Unleash your <span className="text-[#fe8162]">Potential</span> with us
+        </h5>
+        <div
+          className={
+            "text-[#767575] text-[20px] font-medium 2xl:w-[350px] w-[600px] text-center 2xl:text-left mt-6 " +
+            inter.className
+          }
+        >
+          Join our community of ambitious individuals and organizations eager to
+          make a difference.
+        </div>
+        <div className="mt-6 mx-auto 2xl:mx-0 w-fit">
+          <button
+            className={
+              outfir.className +
+              " bg-[#8247ff] relative w-[220px] group justify-center flex rounded-xl text-white font-semibold text-xl px-7 py-3 transition-all  hover:shadow-[0px_0px_0px_10px_#d7c5fc]  "
+            }
+          >
+            <div className="text-center group-hover:-translate-x-4 transition-all ease-in-out duration-300 ">
+              Try Out Now
+            </div>
+            <div className="hidden group-hover:block absolute right-9">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="25"
+                height="25"
+                fill="currentColor"
+                className="bi bi-arrow-right-circle-fill"
+                viewBox="0 0 16 16"
+              >
+                <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z" />
+              </svg>
+            </div>
+          </button>
+        </div>
+      </div>
+      <div className="w-fit  grid place-content-center 2xl:mt-0 mt-24 mx-auto 2xl:mx-0">
+        <div className="space-y-12">
+          <HappyCustomerContainer />
+          <div className="relative w-fit mx-auto">
+            <Image
+              className="absolute"
+              src={
+                "https://framerusercontent.com/images/584hzJDnWCS5SFABKjaCzgDspY.png?scale-down-to=512"
+              }
+              alt="Overlay"
+              width={440}
+              height={170}
+            />
+            <Image
+              src={
+                "https://framerusercontent.com/images/S4kVG1FFzSqxr8zOqpvJRfsBad0.png?scale-down-to=512"
+              }
+              alt="Overlay Emoji"
+              width={440}
+              height={170}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function LinksItems({ children }: { children: React.ReactNode }) {
+  return (
+    <Link
+      href={"/"}
+      className={
+        "text-[16px] font-medium hover:text-[#767575] " + inter.className
+      }
+    >
+      {children}
+    </Link>
+  );
+}
+
+function SocialLinks() {
+  return (
+    <section className="mt-[80px]  pb-[40px] max-w-[1000px] 2xl:max-w-[1200px] mx-auto flex flex-col gap-20">
+      <div className="flex items-baseline justify-between">
+        <Image
+          src={
+            "https://framerusercontent.com/images/tgELERqZ0nObn14bTi418qTbg.png?scale-down-to=512"
+          }
+          className="w-44"
+          alt="COmpanYLogo"
+          width={400}
+          height={100}
+        />
+        <div className="flex flex-col gap-2">
+          <LinksItems>Features</LinksItems>
+          <LinksItems>FAQ</LinksItems>
+          <LinksItems>Pricing</LinksItems>
+          <LinksItems>Testimonials</LinksItems>
+        </div>
+      </div>
+      <div className="pt-10 border-t flex justify-between">
+        <div className={"font-medium w-fit text-[16px] "+inter.className}>© 2022 ManageWise, Inc.</div>
+        <div className="flex gap-3 ">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-instagram"
+            viewBox="0 0 16 16"
+          >
+            <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334" />
+          </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-twitter-x"
+            viewBox="0 0 16 16"
+          >
+            <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865l8.875 11.633Z" />
+          </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-linkedin"
+            viewBox="0 0 16 16"
+          >
+            <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401m-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4" />
+          </svg>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+function Footer() {
+  return (
+    <div className="bg-gradient-to-b from-white to-[#fdf2ed]">
+      <Closing />
+      <SocialLinks />
+    </div>
+  );
 }
 
 export default function Home() {
@@ -704,7 +877,7 @@ export default function Home() {
       <FAQSectiom />
       <PricingPlan />
       <Testimony />
-      <Footer/>
+      <Footer />
     </>
   );
 }
